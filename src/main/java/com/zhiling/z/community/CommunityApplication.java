a6cfdaacf6ef7c606fb28ec76a4a3d6f8a5d6887
180543011 <2668449614@qq.com>
2019-12-20 17:52:09 +0800

    init repository

---
+++ src/main/java/com/zhiling/z/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package com.zhiling.z.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zlhl
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
