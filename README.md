3e2c1cf63b67209c9411e02471db990578ab66a9
180543011 <2668449614@qq.com>
2019-12-20 20:03:11 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## 社区项目，练习使用
-
+ 39社区
 ## 资料
 https://www.bilibili.com/video/av65117012 
 https://spring.io/projects/spring-boot
---
+++ src/main/java/com/zhiling/z/community/config/MyMvcConfig.java
@@ -0,0 +1,19 @@
+package com.zhiling.z.community.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author zlhl
+ */
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+
+}
